cd56aa5e1b2034057c457bd0e645c3ac6da809ba
Dominic Brügger <bruegger@puzzle.ch>
2020-06-04 13:45:16 +0200

    Implement login

--- src/com/puzzleitc/jenkins/command/OpenshiftApplyCommand.groovy
+++ src/com/puzzleitc/jenkins/command/OpenshiftApplyCommand.groovy
@@ -20,34 +20,48 @@ class OpenshiftApplyCommand {
     // - Parameter app Optional?
     // - Mehr als ein Project?
     // - Next Steps
-    Object execute() {
+    void execute() {
         ctx.info("-- openshiftApply --")
         def configuration = ctx.stepParams.getRequired("configuration")
-        def project = ctx.stepParams.getRequired("project")
-        def cluster = ctx.stepParams.getOptional("cluster", null)
-        def app = ctx.stepParams.getOptional("app", null)
+        def project = ctx.stepParams.getRequired("project") as String
+        def cluster = ctx.stepParams.getOptional("cluster", null) as String
+        def app = ctx.stepParams.getOptional("app", null) as String
         def credentialId = ctx.stepParams.getOptional("credentialId", "${project}${DEFAULT_CREDENTIAL_ID_SUFFIX}") as String
-        def saToken = ctx.lookupTokenFromCredentials(credentialId)
-        def ocHome = ctx.tool(DEFAULT_OC_TOOL_NAME);
-        ctx.withEnv(["PATH+OC_HOME=${ocHome}/bin"]) {
-            ctx.openshift.withCluster(cluster) {
-                ctx.openshift.withProject(project) {
-                    ctx.openshift.withCredentials(saToken) {
-                        ctx.echo("openshift whoami: ${ctx.openshift.raw('whoami').out.trim()}")
-                        ctx.echo("openshift cluster: ${ctx.openshift.cluster()}")
-                        ctx.echo("openshift project: ${ctx.openshift.project()}")
 
-                        /*
-                        openshift.raw("convert", "-f", "mongodb.yaml")
-                        */
-                        def result = ctx.openshift.apply(configuration, "-l", "app=${app}", "--prune")
-                        ctx.echo("openshift result action: ${result.actions[0].cmd}")
-                        ctx.echo("openshift result status: ${result.status}")
-                        ctx.echo("openshift result output:\n${result.out}")
-                        ctx.openshift.selector("dc", app).rollout().status()
-                    }
-                }
-            }
+        login(credentialId, cluster, project)
+
+    }
+
+    void login(String credentialId, String cluster, String project) {
+        def saToken = ctx.lookupTokenFromCredentials(credentialId)
+        if (!saToken) {
+            ctx.fail("Token for credentialId '${credentialId}' cannot be found in Jenkins")
+        }
+
+        def exitCode = invokeOcCommand("oc login ${cluster} --insecure-skip-tls-verify=true --token=${saToken}", false, true) as int
+        if (exitCode != 0) {
+            ctx.fail("Login to OpenShift cluster ${cluster} failed: exitCode=${}")
+        } else {
+            ctx.echo("Successfully logged in to OpenShift cluster")
+            ctx.echo("openshift whoami: ${whoami()}")
+            ctx.echo("openshift cluster: ${cluster}")
+        }
+
+        if (project) {
+            ctx.echo(invokeOcCommand("oc project ${project}", true) as String)
+            ctx.echo("openshift project: ${project}")
+        }
+    }
+
+    String whoami() {
+        def out = invokeOcCommand("oc whoami", true) as String
+        out.trim()
+    }
+
+    Object invokeOcCommand(String command, boolean returnStdout = false, boolean returnStatus = false) {
+        def ocHome = ctx.tool(DEFAULT_OC_TOOL_NAME)
+        ctx.withEnv(["PATH+OC_HOME=${ocHome}/bin"]) {
+            ctx.sh(script: command, returnStdout: returnStdout, returnStatus: returnStatus)
         }
     }
 
