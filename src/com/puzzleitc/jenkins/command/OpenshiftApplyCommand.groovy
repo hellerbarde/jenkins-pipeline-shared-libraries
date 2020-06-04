230364e738b209a282ddc381d3db296be3b93d8c
Dominic Brügger <bruegger@puzzle.ch>
2020-06-04 15:20:42 +0200

    Refine oc login

--- src/com/puzzleitc/jenkins/command/OpenshiftApplyCommand.groovy
+++ src/com/puzzleitc/jenkins/command/OpenshiftApplyCommand.groovy
@@ -6,6 +6,7 @@ class OpenshiftApplyCommand {
 
     private static final DEFAULT_CREDENTIAL_ID_SUFFIX = "-cicd-deployer"
     private static final DEFAULT_OC_TOOL_NAME = "oc_3_11"
+    private static final DEFAULT_CLUSTER = "https://openshift.puzzle.ch"
 
     private final PipelineContext ctx
 
@@ -13,18 +14,11 @@ class OpenshiftApplyCommand {
         this.ctx = ctx
     }
 
-    // TODO:
-    // - Direkt oc cli über Shell verwenden?
-    // - Globale Cluster Konfiguration?
-    // - Gehören Labels nicht eher ins Template?
-    // - Parameter app Optional?
-    // - Mehr als ein Project?
-    // - Next Steps
     void execute() {
         ctx.info("-- openshiftApply --")
         def configuration = ctx.stepParams.getRequired("configuration")
         def project = ctx.stepParams.getRequired("project") as String
-        def cluster = ctx.stepParams.getOptional("cluster", null) as String
+        def cluster = ctx.stepParams.getOptional("cluster", DEFAULT_CLUSTER) as String
         def app = ctx.stepParams.getOptional("app", null) as String
         def credentialId = ctx.stepParams.getOptional("credentialId", "${project}${DEFAULT_CREDENTIAL_ID_SUFFIX}") as String
 
@@ -37,20 +31,14 @@ class OpenshiftApplyCommand {
         if (!saToken) {
             ctx.fail("Token for credentialId '${credentialId}' cannot be found in Jenkins")
         }
-
-        def exitCode = invokeOcCommand("oc login ${cluster} --insecure-skip-tls-verify=true --token=gschnabber", false, true) as int
+        ctx.echo("Logging into to OpenShift cluster")
+        def exitCode = invokeOcCommand("oc login ${cluster} --insecure-skip-tls-verify=true --token=${saToken}", false, true) as int
         if (exitCode != 0) {
-            ctx.fail("Login to OpenShift cluster ${cluster} failed: exitCode=${}")
-        } else {
-            ctx.echo("Successfully logged in to OpenShift cluster")
-            ctx.echo("openshift whoami: ${whoami()}")
-            ctx.echo("openshift cluster: ${cluster}")
+            ctx.fail("Logging into OpenShift cluster ${cluster} failed")
         }
 
-        if (project) {
-            ctx.echo(invokeOcCommand("oc project ${project}", true) as String)
-            ctx.echo("openshift project: ${project}")
-        }
+        ctx.echo(invokeOcCommand("oc project ${project}", true) as String)
+        ctx.echo("openshift project: ${project}")
     }
 
     Object invokeOcCommand(String command, boolean returnStdout = false, boolean returnStatus = false) {
